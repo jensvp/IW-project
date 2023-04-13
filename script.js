64738c4c851608d640cc7c18165a021e3c2396d7
Van Parijs Jens <jensvanparijs@hotmail.be>
2023-04-13 14:40:50 +0200

    script deels in orde, css en html ok

--- script.js
+++ script.js
@@ -1,5 +1,114 @@
+    window.onload = function() {
+    let fout = document.getElementById("fout");
+    let extra = document.getElementById("extra");
+    fout.style.display = "none"
+    extra.style.display = "none"};
+    // https://stackoverflow.com/questions/22234219/hide-div-while-the-page-is-loading
+  
+    //const errors = [];
 
-  const alertButton = document.getElementById("validate");
-  alertButton.addEventListener("click", function() {
-    window.alert("This is an alert message.");
-  });
+
+
+   function validateForm()
+   {
+    fout.style.display = "block";
+//     let leeg = false;
+
+//     let voornaam = document.getElementById("voornaam").value;
+//     var a = voornaam;
+//     var b = "voornaam";
+//     checkemptyField(a,b);  
+//     let naam = document.getElementById("naam").value;
+//     var a = naam;
+//     var b = "naam";
+//     checkemptyField(a,b);
+//     let gebruikersnaam = document.getElementById("gebruikersnaam").value;
+//     var a = gebruikersnaam;
+//     var b = "gebruikersnaam";
+//     checkemptyField(a,b);
+//     let email = document.getElementById("email").value;
+//     var a = email;
+//     var b = "email";
+//     checkemptyField(a,b);
+//     let wachtwoord = document.getElementById("ww").value;
+//     var a = wachtwoord;
+//     var b = "wachtwoord";
+//     checkemptyField(a,b);
+//     let checkwachtwoord = document.getElementById("wwcheck").value;
+//     var a = checkwachtwoord;
+//     var b = "herhaal wachtwoord";
+//     checkemptyField(a,b);
+//     let adres = document.getElementById("adres").value;
+//     var a = adres;
+//     var b = "adres";
+//     checkemptyField(a,b);
+//     let land = document.getElementById("land").value;
+//     var a = land;
+//     var b = "land";
+//     checkemptyField(a,b);
+//     let provincie = document.getElementById("provincie").value;
+//     var a = provincie;
+//     var b = "provincie";
+//     checkemptyField(a,b);
+//     let postcode = document.getElementById("postcode").value;
+//     var a = postcode;
+//     var b = "postcode";
+//     checkemptyField(a,b);
+//     if(leeg == true){
+//         displayErrors(leeg, fout);
+//     };
+    
+//     console.log(leeg);
+
+    
+    
+// console.log(errors);
+
+// function checkemptyField(a,b)
+//   {
+    
+//     if (a === "")
+//     {
+        
+//         let boodschap = "het veld " + b + " is vereist.";
+//         errors.push(boodschap);
+//         leeg = true;
+        
+//     }
+    
+    
+//   };
+
+//   function displayErrors(leeg, fout) {
+  
+//     if (leeg == true) {
+//     //   for (let i = 0; i < errors.length; i++) {
+//     //     const error = errors[i];
+//     //     const errorboodschap = document.createElement("p");
+//     //     errorboodschap.textContent = error;
+//     //     fout.appendChild(errorboodschap);
+//     //   }
+//       document.getElementById("fout").style.display = "inline";
+//     } else {
+//       fout.style.display = "none";
+//     }
+// };
+   
+// //   function blanco(fout)
+// //   {
+    
+// //     if(errors.length>0)
+// //     {
+// //         fout = fout + errors;
+// //         show = fout.style.display = "block";
+// //         return show;
+// //     }
+// //     else{
+// //         return fout.style.display = "none";
+// //     }
+// //   }
+     
+};
+
+
+  
