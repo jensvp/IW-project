ca1ea0ebf0acceaeb8314ebb73b4e3dbcdd3e7fb
Van Parijs Jens <jensvanparijs@hotmail.be>
2023-04-15 12:38:30 +0200

    t.e.m.domein

--- script.js
+++ script.js
@@ -8,20 +8,25 @@
     fout1.style.visibility = "hidden";
     goed1.style.visibility = "hidden";
     betalingswijze1.style.visibility = "hidden";
+
+    
 };
     // https://stackoverflow.com/questions/22234219/hide-div-while-the-page-is-loading
   
     
-
+    let teller = 0;
    function validateForm()
    {
-    const errors = [];
+    // const errors = [];
     let leeg = false;
+    let ww1 = false;
+    let ww2 = false;
+    
 
-    let voornaam = document.getElementById("voornaam").value;
-    var a = voornaam;
-    var b = "voornaam";
-    checkemptyField(a,b);  
+    // let voornaam = document.getElementById("voornaam").value;
+    // var a = voornaam;
+    // var b = "voornaam";
+    // checkemptyField(a,b);  
     let naam = document.getElementById("naam").value;
     var a = naam;
     var b = "naam";
@@ -33,20 +38,32 @@
     let email = document.getElementById("email").value;
     var a = email;
     var b = "email";
-    checkemptyField(a,b);
-    // validateEmail(email);
-    // if (validateEmail == false)
-    // {
-    //     errors.push("E-mailadres is niet correct")
-    // }
+    if(checkemptyField(a,b)){
+    
+        if (!validateEmail(email))
+        {
+            errors.push("E-mailadres is niet correct")
+        }
+    };
     let wachtwoord = document.getElementById("ww").value;
     var a = wachtwoord;
     var b = "wachtwoord";
-    checkemptyField(a,b);
+    if(checkemptyField(a,b))
+    {
+        ww1 = true;
+    };
     let checkwachtwoord = document.getElementById("wwcheck").value;
     var a = checkwachtwoord;
     var b = "herhaal wachtwoord";
-    checkemptyField(a,b);
+    if(checkemptyField(a,b))
+    {
+        ww2 = true;
+    };
+    if(ww1 && ww2)
+    {
+        checkPasword(checkwachtwoord,wachtwoord);
+    }
+    
     let adres = document.getElementById("adres").value;
     var a = adres;
     var b = "adres";
@@ -62,35 +79,42 @@
     let postcode = document.getElementById("postcode").value;
     var a = postcode;
     var b = "postcode";
+    checkemptyField(a,b);
+    
+    
 
-    let betaling = document.querySelector('input[name = "flexRadioDefault"]:checked').value;
-    validatePayment(betaling);
+    // let betaling = document.querySelector('input[name = "flexRadioDefault"]:checked').value;
+    // validatePayment(betaling);
     
     if(leeg == true){
         displayErrors(fout);
     } 
-    
+    teller ++;
    
-console.log(errors);
-
+console.log(provincie);
 function checkemptyField(a,b)
   {
     
-    if (a === "")
+    if (a == "" || a == 0 || a == "Kies een provincie")
     {
         
         let boodschap = "het veld " + b + " is vereist.";
         errors.push(boodschap);
         leeg = true;
-        
-    }
-    
+        return false
+    };
+     return true;
     
   };
 
-  function displayErrors(leeg) {
-  
+  function displayErrors() {
+    if (teller > 0)
+    {
+        fout1.innerHTML = "";
+ 
+    }
     
+
       for (let i = 0; i < errors.length; i++) {
         const error = errors[i];
         const errorboodschap = document.createElement("p");
@@ -100,6 +124,7 @@ function checkemptyField(a,b)
       
       fout1.style.visibility = "visible";
     }
+    
 };
 
 function validatePayment(betaling)
@@ -120,29 +145,54 @@ function validatePayment(betaling)
     
 };
 
+function validateEmail(email)
+{
+    //https://www.aspsnippets.com/Articles/Email-validation-without-using-Regular-Expression-in-JavaScript.aspx
+        //Check minimum valid length of an Email.
+        if (email.length <= 2) {
+            return false;
+        }
+        //If whether email has @ character.
+        if (email.indexOf("@") == -1) {
+            return false;
+        }
+ 
+        var parts = email.split("@");
+        var dot = parts[1].indexOf(".");
+        var len = parts[1].length;
+        var dotSplits = parts[1].split(".");
+        var dotCount = dotSplits.length - 1;
+ 
+ 
+        //Check whether Dot is present, and that too minimum 1 character after @.
+        if (dot == -1 || dot < 2 || dotCount > 2) {
+            return false;
+        }
+ 
+        //Check whether Dot is not the last character and dots are not repeated.
+        for (var i = 0; i < dotSplits.length; i++) {
+            if (dotSplits[i].length == 0) {
+                return false;
+            }
+        }
+ 
+        return true;
+    };
+
+    function checkPasword(checkwachtwoord, wachtwoord)
+    {
+        if(wachtwoord.length < 8)
+        {
+            errors.push("Het wachtwoord moet minstens 8 karakters lang zijn.")
+        }
+        
+        else if(wachtwoord != checkwachtwoord)
+        {
+            errors.push("Beide wachtwoorden moeten hetzelfde zijn.")
+        }
+    };
 
 
-// function validateEmail(email)
-// var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-// if (email.value.match(validRegex)) {
-
-//   alert("Valid email address!");
-
-//   document.form1.text1.focus();
-
-//   return true;
-
-// } else {
-
-//   alert("Invalid email address!");
-
-//   document.form1.text1.focus();
-
-//   return false;
-
-// }
-   
 };   
    
 
